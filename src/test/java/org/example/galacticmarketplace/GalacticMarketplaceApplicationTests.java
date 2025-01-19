61545399ac9e7a83e5e18a7f2011360fe76a192e
patronus246 <pespatron.original@gmail.com>
2025-01-19 18:17:04 +0200

    Initial commit

---
+++ src/test/java/org/example/galacticmarketplace/GalacticMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.galacticmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
